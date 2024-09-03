bd710ba6db2156e9b3761979755ff91870e20851
Elias Taolela <37247904+EliasTaolela@users.noreply.github.com>
2024-09-03 17:43:58 +0000

    database access

--- wrapsaPdf/resources/views/departments/index.blade.php
+++ wrapsaPdf/resources/views/departments/index.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5">
-    <h2>Saved Departments</h2>
+<div class="container">
+    <h1>Departments</h1>
+    <a href="{{ route('departments.create') }}" class="btn btn-primary">Add New Department</a>
 
-    <ul class="list-group mt-4">
-        @foreach($departments as $department)
-            <li class="list-group-item">
-                <a href="{{ route('departments.show', $department->id) }}">{{ $department->name }}</a>
-            </li>
-        @endforeach
-    </ul>
+    @if($departments->isEmpty())
+        <p>No departments found.</p>
+    @else
+        <ul class="list-group mt-4">
+            @foreach($departments as $department)
+                <li class="list-group-item">
+                    <a href="{{ route('departments.show', $department->id) }}">{{ $department->name }}</a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
 </div>
 @endsection
